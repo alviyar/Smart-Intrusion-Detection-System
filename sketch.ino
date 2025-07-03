73a8d1133ebd0f4a4be16b287d5c2a201ce54c5d
Alviya Rizwan <120601642+alviyar@users.noreply.github.com>
2025-07-03 11:09:03 +0530

    Create sketch.ino

---
+++ sketch.ino
@@ -0,0 +1,19 @@
+int pirPin = 15;  // PIR sensor output pin
+int ledPin = 2;   // Optional: LED pin
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int motion = digitalRead(pirPin);
+  if (motion == HIGH) {
+    Serial.println("🚨 Motion Detected! Sending alert...");
+    digitalWrite(ledPin, HIGH);
+    delay(1000);  // Avoid spamming
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
